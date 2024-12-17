81c79976db4dea5a91f507f50adb26a1085bce97
mjuncobranco <mjuncobranco@gmail.com>
2024-12-17 15:44:36 +0100

    moviesContext ready, loading & fetching errors controlled, fetching movies from db correctly

--- src/context/MoviesContext.jsx
+++ src/context/MoviesContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState, useEffect } from "react";
+
 import PropTypes from "prop-types";
-//creating movieContext 
+//creating movieContext
 export const MoviesContext = createContext();
 
 export const MoviesProvider = ({ children }) => {
@@ -9,51 +10,36 @@ export const MoviesProvider = ({ children }) => {
   const [loading, setLoading] = useState(true); // loading...
   const [error, setError] = useState(null); // error
 
-
   // Fetch movies just once at the beginning
   const fetchMovies = async () => {
     try {
-      setLoading(true); // Indicar que se está cargando
+      setLoading(true); // controlling loading: if loading, show msg
       setError(null);
       const response = await fetch(
         `${import.meta.env.VITE_API_URL}/home/movies`
       );
       const data = await response.json();
-      if (data.status === "success") {
+      if (data.status === "success") {//fetched movies ok
         setMovies(data.movies);
-        
       } else {
         console.error("Failed to fetch movies");
       }
-    
     } catch (error) {
-      console.error("Error fetching movies", error);
+      console.error("Error fetching movies", error); //failed to fetch movies
       setError(true);
-    }finally {
-      setLoading(false); // Indicar que la carga terminó
+    } finally {
+      setLoading(false); // loading complete msg
     }
-  
   };
 
-  // Add or remove a movie from favorites
-  const toggleFavorite = (movieId) => {
-    setFavorites((prevFavorites) => {
-      if (prevFavorites.includes(movieId)) {
-        // If the movie is already in favorites, remove it
-        return prevFavorites.filter((id) => id !== movieId);
-      } else {
-        // If the movie is not in favorites, add it
-        return [...prevFavorites, movieId];
-      }
-    });
-  };
-  
   useEffect(() => {
     fetchMovies();
   }, []);
 
   return (
-    <MoviesContext.Provider value={{ movies, favorites, loading,error, toggleFavorite }}>
+    <MoviesContext.Provider
+      value={{ movies, favorites, loading, error, setFavorites }}
+    >
       {children}
     </MoviesContext.Provider>
   );
